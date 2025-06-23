3859e0aefd1f2f5c96a12c4c7ad6bccd164c7076
shafia-dal <shafia.dal@operisoft.com>
2025-06-23 07:13:07 +0000

    edit after install

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -6,6 +6,9 @@ cd /home/ubuntu/project
 echo "Ensuring correct file ownership..."
 sudo chown -R ubuntu:ubuntu /home/ubuntu/project
 
+echo "Installing Node.js tools (if not already)..."
+command -v yarn >/dev/null 2>&1 || sudo npm install -g yarn
+command -v pm2 >/dev/null 2>&1 || sudo npm install -g pm2
 
 echo "Installing Node.js dependencies..."
 yarn cache clean
